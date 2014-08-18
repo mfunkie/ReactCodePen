861fdcd57af8dcc6cc40ce2f914d7ac7fe935e1b
Mark Funk <mark@boomtownroi.com>
2014-08-18 17:42:45 -0400

    Updating attribute setting

--- react-codepen.js
+++ react-codepen.js
@@ -8,6 +8,6 @@ if (window.addEventListener) {
 function runScripts() {
   console.log(document.querySelectorAll('body script:not([src])'));
   Array.prototype.forEach.call(document.querySelectorAll('body script:not([src])'), function(element) {
-    element.attributes.type = 'text/jsx';
+    element.setAttribute('type', 'text/jsx');    
   });
 };
