8d768a5e84f08baee739e42676a3affb5c8d2a0c
Mark Funk <mark@boomtownroi.com>
2014-08-18 17:27:23 -0400

    Updating with console logging

--- react-codepen.js
+++ react-codepen.js
@@ -1,3 +1,4 @@
+console.log(document.querySelectorAll('body script:not([src])'));
 Array.prototype.forEach.call(document.querySelectorAll('body script:not([src])'), function(element) {
   element.attributes.type = 'text/jsx';
 });
