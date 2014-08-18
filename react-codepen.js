1c92f53d87c7a06004a9d48299eaba9ca4df015a
Mark Funk <mark@boomtownroi.com>
2014-08-18 17:30:48 -0400

    Updating script to run on window load, hopefully before jsx transformer

--- react-codepen.js
+++ react-codepen.js
@@ -1,4 +1,13 @@
-console.log(document.querySelectorAll('body script:not([src])'));
-Array.prototype.forEach.call(document.querySelectorAll('body script:not([src])'), function(element) {
-  element.attributes.type = 'text/jsx';
-});
+
+if (window.addEventListener) {
+  window.addEventListener('DOMContentLoaded', runScripts, false);
+} else {
+  window.attachEvent('onload', runScripts);
+}
+
+function runScripts() {
+  console.log(document.querySelectorAll('body script:not([src])'));
+  Array.prototype.forEach.call(document.querySelectorAll('body script:not([src])'), function(element) {
+    element.attributes.type = 'text/jsx';
+  });
+};
