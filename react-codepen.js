daaf2eaa31e5887b440ab340f136b8ffb85db971
Mark Funk <mark@boomtownroi.com>
2014-08-18 22:20:58 -0400

    Wrapping in IIFE to not pollute global scope.  Adding function name to listener function

--- react-codepen.js
+++ react-codepen.js
@@ -1,12 +1,15 @@
+(function() {
+  function runScripts() {
+    var bodyScripts = 'body script:not([src])';
+    Array.prototype.forEach.call(document.querySelectorAll(bodyScripts), function setJSXType(element) {
+      element.setAttribute('type', 'text/jsx');    
+    });
+  };
 
-if (window.addEventListener) {
-  window.addEventListener('DOMContentLoaded', runScripts, false);
-} else {
-  window.attachEvent('onload', runScripts);
-}
+  if (window.addEventListener) {
+    window.addEventListener('DOMContentLoaded', runScripts, false);
+  } else {
+    window.attachEvent('onload', runScripts);
+  }
+})();
 
-function runScripts() {
-  Array.prototype.forEach.call(document.querySelectorAll('body script:not([src])'), function(element) {
-    element.setAttribute('type', 'text/jsx');    
-  });
-};
